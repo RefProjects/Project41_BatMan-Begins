4fbccfa9e27c3031fe30cb96078f0ba5912aaa17
RefProjects <73771618+RefProjects@users.noreply.github.com>
2020-11-01 12:43:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+//Create the variables below
+
+function preload(){
+    
+
+}
+
+function setup(){
+    createCanvas(1000,600);
+
+   
+    
+}
+
+function draw(){
+    background(0); 
+
+    //Call the function rainDrops below
+    
+
+    //Write the if statement to move the man below
+   
+   
+    drawSprites();
+}   
+
+//Create the rainDrops function below
+
+
+
+    
